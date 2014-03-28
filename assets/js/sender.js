65b72e382f55716c73f08a8e1f5e36b96aaa0491
Oliver Nürnberger <o.nuernberger@blue-silver.com>
2014-03-28 23:53:27 +0100

    change ip range

--- assets/js/sender.js
+++ assets/js/sender.js
@@ -6,6 +6,6 @@ var message = new Buffer(process.argv[2]);
 var client = dgram.createSocket("udp4");
 client.bind();
 client.setBroadcast(true);
-client.send(message, 0, message.length, 8282, "192.168.191.255", function(err, bytes) {
+client.send(message, 0, message.length, 8282, "192.168.0.255", function(err, bytes) {
     client.close();
 });
